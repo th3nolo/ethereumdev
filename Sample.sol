bff01acd71dd6cd147d1f86b69fcc17623cf18f9
Manuel Parra <th3nolo@gmail.com>
2021-11-02 20:33:08 -0400

    First Commit

--- Sample.sol
+++ Sample.sol
@@ -1,2 +1,13 @@
 pragma pragma solidity ^0.8.6;
 
+contract Samplegithub {
+    function get() public constant returns (uint) {
+        return 42;
+    }
+
+    function get(uint x) public constant returns (uint) {
+        return x;
+    }
+    
+}
+
